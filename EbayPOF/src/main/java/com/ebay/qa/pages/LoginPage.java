8d19e261857765f6cfd8f17136998c7c148cef6d
afzal <afzal@LAPTOP-JF515C61.gateway.pace.com>
2019-05-18 17:46:43 -0400

    First - Commit

---
+++ EbayPOF/src/main/java/com/ebay/qa/pages/LoginPage.java
@@ -0,0 +1,43 @@
+package com.ebay.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.PageFactoryFinder;
+
+import com.ebay.qa.base.TestBase;
+
+public class LoginPage extends TestBase{
+
+	@FindBy(xpath="//input[@id='userid']")
+	WebElement username;
+
+	
+	@FindBy(xpath="//input[@id='pass']")
+	WebElement password;
+	
+	@FindBy(xpath="//button[@id='sgnBt']")
+	WebElement SingInBtn;
+	
+	
+	public LoginPage() throws Exception {
+		super();
+		
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	//actions//
+	
+	
+	public HomePage verifyLoginIn(String usernm, String psswrd) throws Exception
+	{
+		username.sendKeys(usernm);
+		password.sendKeys(psswrd);
+		SingInBtn.click();
+		
+		return new HomePage();
+		
+	}
+
+}
